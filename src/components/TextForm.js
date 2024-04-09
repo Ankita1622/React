4a636746cba8ff260f2fe56977b43ce0ec6626a0
Ankita1622 <ankitakanara@gmail.com>
2024-04-09 16:54:12 +0530

    Enable dark mode and light mode using state hook

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -26,8 +26,10 @@ export default function TextForm(props) {
             <label forname="mybox" className="form-label">Example textarea</label>
             <textarea className="form-control" value = {text} id="mybox" onChange={handleOnChange} rows="8"></textarea>
         </div>
+        
         <button type="button" className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
         <button type="button" className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to lowercase</button>
+
         <div className="my-3">
             <h2>Your text summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
